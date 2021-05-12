f0e15316be49dd5ecfaf8633269ffa95e9fa8658
schckngs <andreahilborn@gmail.com>
2021-05-11 17:14:29 -0700

    Code to assemble and plot data

---
+++ NRT_SST_Calc.R
@@ -0,0 +1,138 @@
+# Near real-time plots of SST and anomalies using CoastWatch Data
+library(rerddap)
+library(dplyr)
+library(ggplot2)
+library(lubridate)
+theme_set(theme_bw())
+
+# Area
+latlim = c(30,61.5) #c(10,60)
+lonlim = c(-160,-120)
+
+# Time limit:
+end_date <- Sys.Date() - 2
+start_date = end_date-6 #-6 for 7 days, -13 for 2 weeks 
+timelim = c(start_date, end_date)
+print(timelim)
+print(paste(yday(end_date)-yday(start_date)+1,"days"))
+
+# MODISA ####
+# Datasets: 
+# MODIS-Aqua SST masked, 2003-present,approx 4km resolution
+# Note that sci qual has a 120 day delay, NRT has 2 day delay
+# erdMH1sstd1dayR20190 (1-day SST, masked) 
+# erdMH1sstd7dayR20190SQ (8-day SST, science quality) - Says 90 days out of date but current to just over 1 month old
+# erdMH1sstdmdayR20190 (Monthly SST)
+# For monthly composites:
+# erdMH1sstdmdayR20190SQ (Current year science quality: lag of 120 days)
+
+
+# MODISA rolling 7-day average ####
+sstInfo <- info("erdMH1sstd1dayR20190", url = "https://coastwatch.pfeg.noaa.gov/erddap/")
+sstdata <- griddap(sstInfo, latitude = latlim, longitude = lonlim, 
+                    time = timelim)
+print(paste(length(unique(sstdata$data$time)),"DAYS FOUND"))
+
+sstdata_7day <- sstdata$data %>% 
+  filter(!is.na(sstMasked)) %>%
+  group_by(lat, lon) %>% 
+  summarise(sst_7day = mean(sstMasked, na.rm=T),
+            sst_7daysd = sd(sstMasked, na.rm=T),
+            sst_7dayn = sum(!is.na(sstMasked))) %>%
+  ungroup() %>% 
+  mutate(start_date = start_date,
+         end_date = end_date)
+saveRDS(sstdata_7day, "data/MODISA_SST7day_rollingavgbackup_current.rds")
+
+# MODISA 7-day climatology ####
+sstvar = info("erdMH1sstd1dayR20190SQ", url = "https://coastwatch.pfeg.noaa.gov/erddap/")
+yr7days <- list()
+ct=1
+for (i in 2003:2020) {
+  start = as.Date(paste0(i, yday(start_date)), format = "%Y%j")
+  end = as.Date(paste0(i, yday(end_date)), format = "%Y%j")
+  timesub = c(start, end)
+  print(timesub)
+  sstyr <- griddap(sstvar, latitude = latlim, longitude = lonlim, 
+                      time = timesub)
+  print(paste(length(unique(sstyr$data$time)), "DAYS FOUND"))
+  roll7 = sstyr$data %>%
+    filter(!is.na(sstMasked)) %>%
+    group_by(lat, lon) %>%
+    summarise(sst_7day = mean(sstMasked, na.rm=T),
+              sst_7dayn = sum(!is.na(sstMasked))) %>%
+    ungroup()
+  yr7days[[ct]] <- roll7
+  ct=ct+1
+}
+yr7days <- do.call(rbind, yr7days)
+
+yr7days <- yr7days %>% 
+  filter(!is.na(sst_7day)) %>% 
+  group_by(lat, lon) %>% 
+  summarise(sst_7day_clim = mean(sst_7day, na.rm=T),
+            sst_7day_climsd = sd(sst_7day, na.rm=T),
+            sst_7day_climn = sum(sst_7dayn, na.rm = T)) %>% 
+  ungroup()
+saveRDS(object = yr7days, file = "data/MODISA_SST7day_rollingavgbackup_climatology.rds")
+rm(roll7,sstyr, sstInfo)
+gc()
+
+
+
+# OI Data - Current and 30 year climatology (1990-2020) ####
+# Datasets: 
+# ncdcOisst21NrtAgg_LonPM180 (2020-present, 4-day lag)
+# ncdcOisst21Agg_LonPM180 (1981-present, 17-day lag)
+
+# OI Rolling 7-day average ####
+sstInfo = info("ncdcOisst21NrtAgg_LonPM180", url = "https://coastwatch.pfeg.noaa.gov/erddap/")
+sstdata <- griddap(sstInfo, latitude = latlim, longitude = lonlim, 
+                    time = timelim)
+print(paste(length(unique(sstdata$data$time)),"DAYS FOUND"))
+
+sstdata_7day <- sstdata$data %>% 
+  filter(!is.na(sst)) %>%
+  group_by(lat, lon) %>% 
+  summarise(sst_7day = mean(sst, na.rm=T),
+            sst_7daysd = sd(sst, na.rm=T),
+            sst_7dayn = sum(!is.na(sst))) %>%
+  ungroup() %>% 
+  mutate(start_date = start_date,
+         end_date = end_date)
+saveRDS(sstdata_7day, "data/OI_SST7day_rollingavgbackup_current.rds")
+
+# OI 7-day climatology ####
+sstvar = info("ncdcOisst21Agg_LonPM180", url = "https://coastwatch.pfeg.noaa.gov/erddap/")
+yr7days <- list()
+ct=1
+for (i in 1991:2020) {
+  start = as.Date(paste0(i, yday(start_date)), format = "%Y%j")
+  end = as.Date(paste0(i, yday(end_date)), format = "%Y%j")
+  timesub = c(start, end)
+  print(timesub)
+  sstyr <- griddap(sstvar, latitude = latlim, longitude = lonlim, 
+                   time = timesub)
+  print(paste(length(unique(sstyr$data$time)), "days"))
+  roll7 = sstyr$data %>%
+    filter(!is.na(sst)) %>%
+    group_by(lat, lon) %>%
+    summarise(sst_7day = mean(sst, na.rm=T),
+              sst_7dayn = sum(!is.na(sst))) %>%
+    ungroup()
+  yr7days[[ct]] <- roll7
+  ct=ct+1
+}
+yr7days <- do.call(rbind, yr7days)
+
+yr7days <- yr7days %>% 
+  filter(!is.na(sst_7day)) %>% 
+  group_by(lat, lon) %>% 
+  summarise(sst_7day_clim = mean(sst_7day, na.rm=T),
+            sst_7day_climsd = sd(sst_7day, na.rm=T),
+            sst_7day_climn = sum(sst_7dayn),
+            sst_7day_90 = quantile(sst_7day, probs = 0.9)) %>% 
+  ungroup()
+saveRDS(object = yr7days, file = "data/OI_SST7day_rollingavgbackup_climatology.rds")
+rm(roll7,sstyr)
+gc()
---
+++ NRT_SST_Plots_OI.R
@@ -0,0 +1,99 @@
+library(dplyr)
+library(ggplot2)
+library(mapdata)
+theme_set(theme_bw())
+reg = map_data("world2Hires")
+reg = subset(reg, region %in% c('Canada', 'USA'))
+reg$long = (360 - reg$long)*-1
+
+
+#######
+datavar = "OI"
+#######
+datasource = "Data source: https://doi.org/10.25921/RE9P-PT57"
+
+# Region limits:
+latlim = c(30,61.5)
+lonlim = c(-160,-120)
+
+# Aesthetics:
+# "Boiling cauldron of death" palette 
+# Re: Charles - Similar to pals::brewer.rdylbu 
+death_cauldron = rev(c("#A50026", "#EA5839", "#F67B49", "#FB9F5A", "#FDBE70","#FDDA8A", "#FFFFBF","#EDF8DE", #reds
+                       "#DAF0F6", "#BCE1EE", "#9ECFE3", "#80B6D6", "#649AC7", "#4A7BB7", "#3C59A6", "#313695"))
+# Plot rolling 7-day composite for datavar 
+# 7-day mean, sd, N
+curr7days <- readRDS(paste0("data/",datavar,"_SST7day_rollingavgbackup_current.rds"))
+# Climatological mean, sd, N
+clim7days <- readRDS(paste0("data/",datavar,"_SST7day_rollingavgbackup_climatology.rds"))
+
+curr_clim <- full_join(curr7days, clim7days, by = c("lon","lat"))
+curr_clim$diff_7day_deg <- curr_clim$sst_7day-curr_clim$sst_7day_clim
+if (datavar == "OI") {
+  curr_clim$sd_1.3_pos <- (curr_clim$sst_7day_clim+(curr_clim$sst_7day_climsd*1.29))
+  curr_clim$sd_1.3_neg <- (curr_clim$sst_7day_clim-(curr_clim$sst_7day_climsd*1.29))
+  curr_clim$sd_above <- curr_clim$sd_1.3_pos-curr_clim$sst_7day
+  curr_clim$perc90_above <- curr_clim$sst_7day_90-curr_clim$sst_7day
+}
+start = unique(curr_clim$start_date)
+end = unique(curr_clim$end_date)
+start = start[!is.na(start)]
+end = end[!is.na(end)]
+
+# Most recent rolling 7-day average ####
+curr_clim %>% 
+  filter(!is.na(sst_7day)) %>% 
+  ggplot() +
+  geom_tile(aes(x = lon, y = lat, fill = sst_7day)) +
+  scale_fill_gradientn(colours = pals::jet(50), limits=c(0,25), breaks = c(5,10, 15, 20)) +
+  geom_contour(aes(x = lon, y = lat, z = sst_7day), size = 0.5,
+               breaks = c(5,10, 15, 20), colour = "black") +
+  guides(fill = guide_colorbar(barheight = 12, 
+                               ticks.colour = "black", ticks.linewidth = 1.5,
+                               frame.colour = "black", frame.linewidth = 1.5)) +
+  theme(legend.position = "right") +
+  coord_quickmap(xlim = lonlim, ylim = latlim, expand = F) +
+  labs(fill = 'SST (°C)',
+       title = paste(start, "to", end,"Mean Day SST"),
+       subtitle = paste(datavar,"NRT Sea Surface Temperature"),
+       caption = datasource) + xlab(NULL) + ylab(NULL) +
+  scale_y_continuous(breaks = seq(min(latlim), max(latlim), 5)) +
+  scale_x_continuous(breaks = seq(min(lonlim), max(lonlim),5)) +
+  geom_polygon(data = reg, aes(x = long, y = lat, group = group), fill = "grey70", colour = "grey40", size = 0.5)
+
+ggsave(filename = paste0("figures/SST_",datavar,"_7-day_rollingavg_",end,".png"), 
+       device = "png", scale = 1.9, height = 3.5, width = 3.5, units = "in")
+ggsave(filename = paste0("figures/SST_",datavar,"_7-day_rollingavg.png"), 
+       device = "png", scale = 1.9, height = 3.5, width = 3.5, units = "in", dpi=250)
+
+# 7-day climatology anomaly ####
+curr_clim$diff_7day_deg[curr_clim$diff_7day_deg > 3] = 3
+curr_clim$diff_7day_deg[curr_clim$diff_7day_deg < -3] = -3
+curr_clim %>% 
+  filter(!is.na(diff_7day_deg)) %>% 
+  ggplot() +
+  geom_tile(aes(x = lon, y = lat, fill = diff_7day_deg)) +
+  scale_fill_gradientn(colours = death_cauldron, 
+                       limits = c(-3,3), breaks = seq(-3,3,1)) +
+  # geom_contour(aes(x = lon, y = lat, z = perc90_above), size = 0.5,
+  #              breaks = c(0), colour = "green") +
+  geom_contour(aes(x = lon, y = lat, z = sd_above), size = 0.5,
+               breaks = c(0), colour = "black") +
+  guides(fill = guide_colorbar(barheight = 12, 
+                               ticks.colour = "black", ticks.linewidth = 1.5, 
+                               frame.colour = "black", frame.linewidth = 1.5)) +
+  theme(legend.position = "right",panel.background = element_rect(fill = "grey80")) +
+  coord_quickmap(xlim = lonlim, ylim = latlim, expand = F) +
+  labs(fill = 'Anomaly (°C)',
+       title = paste(start, "to", end,"SST Anomaly"),
+       subtitle = paste(datavar,"NRT Sea Surface Temperature Anomaly"),
+       caption = datasource) +
+  xlab(NULL) + ylab(NULL) +
+  scale_y_continuous(breaks = seq(min(latlim), max(latlim), 5)) +
+  scale_x_continuous(breaks = seq(min(lonlim),max(lonlim),5)) +
+  geom_polygon(data = reg, aes(x = long, y = lat, group = group), fill = "grey70", colour = "grey40", size = 0.5)
+
+ggsave(filename = paste0("figures/SST_",datavar,"_7-day_rollingavg_anom_",end,".png"), 
+       device = "png", scale = 1.9, height = 3.5, width = 3.5, units = "in")
+ggsave(filename = paste0("figures/SST_",datavar,"_7-day_rollingavg_anom.png"), 
+       device = "png", scale = 1.9, height = 3.5, width = 3.5, units = "in", dpi=250)
